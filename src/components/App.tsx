c35b530a971197db90c32fd889dfa9bbe1d40182
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-06-05 08:08:27 +0200

    Initalized RC Desktop Application

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <h3>Rocket.Chat Standalone Desktop Application</h3>
+    </div>
+  );
+}
+
+export default hot(App);
---
+++ webpack.main.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: "./src/main/main.ts",
+  module: {
+    rules: require("./webpack.rules"),
+  },
+};
