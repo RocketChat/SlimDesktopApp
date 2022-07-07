e4fda404bf26d5393711a108b9d44a9df9234978
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-07-07 17:41:54 +0200

    feat(realtime-login): Implement a method to login via the websocket

--- src/util/login.util.ts
+++ src/util/login.util.ts
@@ -1,11 +1,31 @@
-import { api } from '@rocket.chat/sdk';
+import { api, driver } from '@rocket.chat/sdk';
+import { LoginResultAPI, LoginCredentials, LoginResultRealtime } from '../interfaces/login';
+import { setAuthToken, getAuthToken } from './authToken.util';
 
 async function loginWithPassword(){
     const email: string = process.env.REACT_APP_EMAIL || "";
-    const password:string = process.env.REACT_APP_PASSWORD || "";
-    await api.login({email, password});
+    const password: string = process.env.REACT_APP_PASSWORD || "";
+    const user: LoginCredentials = {
+        email,
+        password
+    };
+    const result: LoginResultAPI = await api.login(user);
+
+    if(result.status == 'success'){
+        setAuthToken(result.data.authToken);
+    }
+}
+
+async function realTimeLoginWithAuthToken(){
+
+    await driver.connect({ host: "open.rocket.chat", useSsl: true});
+    const res: LoginResultRealtime = await driver.asyncCall("login", [{ "resume": getAuthToken() }]);
+
+    if(!res.id){
+        return new Error('Failed login via Auth Token');
+    }
 
 }
 
 
-export { loginWithPassword };
+export { loginWithPassword, realTimeLoginWithAuthToken };
