fa6ec05cb09c09c9414b0c42b8ee3dac38920fcf
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-07-19 00:59:50 +0200

    feat: Integrate method of cleaning the URL with the realtime login

--- src/util/login.util.ts
+++ src/util/login.util.ts
@@ -1,6 +1,7 @@
 import { api, driver } from '@rocket.chat/sdk';
 import { LoginResultAPI, LoginCredentials, LoginResultRealtime } from '../interfaces/login';
 import { setAuthToken, getAuthToken } from './authToken.util';
+import { cleanURLFromHttp } from './main.util';
 
 async function loginWithPassword(){
     const email: string = process.env.REACT_APP_EMAIL || "";
@@ -17,8 +18,12 @@ async function loginWithPassword(){
 }
 
 async function realTimeLoginWithAuthToken(){
+    const host = process.env.ROCKETCHAT_URL;
+    const useSsl = (process.env.ROCKETCHAT_USE_SSL)
+    ? ((process.env.ROCKETCHAT_USE_SSL || '').toString().toLowerCase() === 'true')
+    : ((host || '').toString().toLowerCase().startsWith('https'));
 
-    await driver.connect({ host: "open.rocket.chat", useSsl: true});
+    await driver.connect({ host: cleanURLFromHttp(host), useSsl});
     const res: LoginResultRealtime = await driver.asyncCall("login", [{ "resume": getAuthToken() }]);
 
     if(!res.id){
