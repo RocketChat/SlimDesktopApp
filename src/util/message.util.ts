a23fa78c9a943de6265bae1ecc40616a6f92084b
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-07-21 05:46:20 +0200

    refactor: Refactor message util file with new sdk version

--- src/util/message.util.ts
+++ src/util/message.util.ts
@@ -1,25 +1,31 @@
-import { driver } from '@rocket.chat/sdk';
-import { RealtimeAPIMessage } from '../interfaces/message';
+import sdk from "../sdk";
+import { RealtimeAPIMessage } from "../interfaces/message";
 
 async function sendTextMessage(roomId: string | undefined, messageText: string) : Promise<boolean> {
     if(!roomId) throw new Error('Room ID Not Found!');
-    let res: RealtimeAPIMessage = await driver.asyncCall("sendMessage", [{
-        rid: roomId,
-        msg: messageText
-    }]);
-    return res._id ? true : false;
+    let res: any = await sdk.post("chat.sendMessage", {
+        message:{
+            rid: roomId,
+            msg: messageText
+        }
+    });
+    return res.success ? true : false;
 }
 
-async function deleteMessageById(messageId: string) {
-    await driver.asyncCall("deleteMessage", [{
-        _id: messageId
-    }]);
+async function deleteMessageById(messageId: string, roomId: string) {
+    await sdk.post("chat.delete", {
+        msgId: messageId,
+        roomId
+    });
 }
 
 async function editTextMessage(message: RealtimeAPIMessage, newMessageText: string) : Promise<boolean> {
     let newMessage: RealtimeAPIMessage = message;
-    newMessage.msg = newMessageText;
-    await driver.asyncCall("updateMessage", [newMessage]);
+    await sdk.post("chat.update", {
+        roomId: newMessage.rid,
+        msgId: newMessage._id,
+        text: newMessageText
+    });
     return true;
 }
 
