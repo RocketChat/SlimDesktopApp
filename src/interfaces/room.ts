2a7ce34df7236944e4b684d6ff2352b1b07b13b1
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-07-23 10:50:52 +0200

    fix: Fix errors in room & message interfaces

--- src/interfaces/room.ts
+++ src/interfaces/room.ts
@@ -1,10 +1,11 @@
+import { RealtimeAPIMessage } from "./message";
 export interface Room {
     _id: string;
 
     name: string;
     fname?: string;
 
-    lastMessage?: string;
+    lastMessage?: RealtimeAPIMessage;
     lm?: string;
     lastMessageDate?: string;
 
