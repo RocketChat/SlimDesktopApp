7421984fc7463203e089bb238a525bf2ce8bba1f
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-07-25 10:11:59 +0200

    feat: Integrate server methods with the sdk initializer

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,11 +1,13 @@
 import { Rocketchat } from '@rocket.chat/sdk';
 import { cleanURLFromSlash } from "../util/main.util";
+import { setCurrentServer } from "../util/server.util";
 
 const host = cleanURLFromSlash(process.env.ROCKETCHAT_URL);
 const useSsl = (process.env.ROCKETCHAT_USE_SSL)
 ? ((process.env.ROCKETCHAT_USE_SSL || '').toString().toLowerCase() === 'true')
 : ((host || '').toString().toLowerCase().startsWith('https'));
 
+setCurrentServer(host);
 const sdk = new Rocketchat({ host, protocol: 'ddp', useSsl, reopen: 20000 });
 
 export default sdk;
