1fda3e694e55ce1c2d8b18c5bd1bfd4af2dc90d8
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-08-03 15:52:00 +0200

    feat: Integrate redux store with the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,24 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { hot } from "react-hot-loader/root";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Login from './Login/Login'
 import ChatList from "./ChatsList/ChatList";
 import ChatWindow from "./ChatWindow/ChatWindow";
+import store from "../state/store";
 import "./style.css";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Router>
         <Routes>
           <Route path='/' exact element={<Login/>}/>
           <Route path="/list" element={<ChatList />} />
-          <Route path="/chat/:id" element={<ChatWindow />} />
+          <Route path="/chat/:id" element={<ChatWindow isThread={false} />} />
         </Routes>
       </Router>
-    </div>
+    </Provider>
   );
 }
 
