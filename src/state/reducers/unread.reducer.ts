f30ace78cba55c7a7521a07c15476f072c525d4b
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-08-25 16:11:00 +0200

    feat: Count number of unread notifications

--- src/state/reducers/unread.reducer.ts
+++ src/state/reducers/unread.reducer.ts
@@ -2,16 +2,26 @@ interface RoomsStatusMap {
     [_id: string]: any
 }
 
-const reducer = (state: {count:Number, rooms:RoomsStatusMap} | null = null, action: {type: string, room: any, payload: any}) => {
+const reducer = (state: {count:number, rooms:RoomsStatusMap} | null = null, action: {type: string, room: any, payload: any}) => {
     switch(action.type){
         case "update":
             state = {count:0, rooms:{...action.payload}};
+            for(let roomId in state.rooms) if(state.rooms[roomId].unread) state.count++;
             return {...state};
         case "read":
-            if(state && state.rooms[action.room.rid]) state.rooms[action.room.rid].alert = false, state.rooms[action.room.rid].unread = 0;
+            if(state && state.rooms[action.room.rid]) {
+                state.rooms[action.room.rid].alert = false, state.rooms[action.room.rid].unread = 0;
+                state.count = 0;
+                for(let roomId in state?.rooms) if(state?.rooms[roomId].unread) state.count++;
+            }
+
             return {...state};
         case "unread":
-            if(state && state.rooms[action.room.rid]) state.rooms[action.room.rid].alert = true, state.rooms[action.room.rid].unread ++;
+            if(state && state.rooms[action.room.rid]) {
+                state.rooms[action.room.rid].alert = true, state.rooms[action.room.rid].unread ++;
+                state.count = 0;
+                for(let roomId in state?.rooms) if(state?.rooms[roomId].unread) state.count++;
+            }
             return {...state};
         default:
             return state;
